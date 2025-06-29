a0f6da16aefc94fbd7b033d9d8c1701bd75900ce
MelissaMelendez15 <melissamelendez15@gmail.com>
2025-06-29 18:47:57 +0000

    chore: se agrega un print a list.py para forzar un cambio

--- src/list.py
+++ src/list.py
@@ -4,6 +4,7 @@ import todoList
 
 
 def list(event, context):
+    print("⚙️ Lambda list() fue invocada — Versión 1.1")
     # fetch all todos from the database
     result = todoList.get_items()
     # create a response
